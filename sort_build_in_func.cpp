2bfc3562e54e1804790a8d98b772962fb66128bf
sumon <debnathsumon1708@gmail.com>
2023-05-30 05:29:07 +0600

    first commit

---
+++ sort_build_in_func.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+        // sorting build in function 
+        // small to big
+    sort(arr, arr+n);
+
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+            // now greater to smaller
+    sort(arr, arr+n, greater<int>());
+
+    for(int i=0; i<n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
